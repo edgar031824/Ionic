069f8310001a847cd6e131498e1b7ca7a91e9adc
Edgar Hernandez <edgar031824@Edgars-MacBook-Pro.local>
2018-09-28 13:19:42 -0500

    se agrega elementos a vista de agregarPage

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,13 +1,29 @@
 import { Component } from "@angular/core";
 import { DeseosService } from "../../services/deseos.service";
-import { Lista} from "../../models/lista.model";
+import { NavParams } from "ionic-angular";
+import { ListaItem,Lista } from "../../models";
 
 @Component({
     selector:"agregar-page",
     templateUrl:"agregar.component.html"
 })
 export class AgregarPage{
-
- constructor(public DeseosService:DeseosService){
- }
+    obList:Lista;
+    sbItemName:string="";
+    constructor(public DeseosService:DeseosService,private navParams:NavParams){
+        const sbTitle=this.navParams.get("titulo");
+        this.obList= new Lista(sbTitle);
+    }
+    
+    AgregarItem(){
+        if(this.sbItemName.length===0){
+            return;       
+        } 
+        const obNewItem= new ListaItem(this.sbItemName);   
+        this.obList.item.push(obNewItem);    
+        this.sbItemName="";
+    }
+    ActualizarCheck(iobItem){
+      iobItem.completado=!iobItem.completado;
+    }
 }
--- src/pages/pendientes/pendientes.component.ts
+++ src/pages/pendientes/pendientes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { DeseosService } from "../../services/deseos.service";
 import { Lista } from "../../models";
-import { NavController, NavControllerBase } from "ionic-angular";
+import { NavController, AlertController} from "ionic-angular";
 import { AgregarPage } from "../agregar/agregar.component";
 
 @Component({
@@ -10,14 +10,36 @@ import { AgregarPage } from "../agregar/agregar.component";
 })
 
 export class PendientesPage {
-	constructor(public _DeseosServices: DeseosService,private navCtrl:NavController) { };
-
+	constructor(public _DeseosServices: DeseosService,private navCtrl:NavController,private alertCtrl:AlertController) { };
+	
 	itemSelected(iobList: Lista) {
-	console.log('hola');
+		console.log('hola');
 	}
 	AgregarLista(){
-		/*con el push se va a la vista sugerida*/
-     this.navCtrl.push(AgregarPage) ;
+		const alertCtrl=this.alertCtrl.create({
+			title: 'Nueva lista',
+			message: "Nombre de la nueva lista que desea ingresar",
+			inputs: [{
+				name: 'titulo',
+				placeholder: 'Nombre de la lista'
+			}],
+			buttons:[
+				{
+					text:"Cancelar"
+				},
+				{
+					text:"Agregar",
+					handler:(iobData)=>{
+						if(iobData.length===0){
+							return;
+						}
+						/*se va a la vista agregarPage y se le envían parámetros*/
+						this.navCtrl.push(AgregarPage,{
+							titulo:iobData.titulo
+						});
+					}
+				}]
+			});
+			alertCtrl.present();
+		}
 	}
-
-}
